5da116dcd62ab35308fae0321be5cad8fdba4c19
singk <2962571236@qq.com>
2024-11-20 14:56:30 +0800

    Delete booklibrary directory

--- booklibrary/makes.c
+++
@@ -1,5 +0,0 @@
-#include <stdlib.h>
-int main(){
-    system("clang ./main.c -Iinclude ./lib/connect/bookStack.c ./lib/connect/Links.c ./lib/storage/basic.c ./lib/storage/filemake/filesto.c ./lib/storage/sto.c -o first.exe");
-    return 0;
-}
