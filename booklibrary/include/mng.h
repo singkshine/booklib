e5ec03ba407f5bcdab6e93147ea49e07e71ad7cb
singk <2962571236@qq.com>
2024-12-10 13:33:38 +0800

    Delete booklibrary directory

--- booklibrary/include/mng.h
+++
@@ -1,9 +0,0 @@
-#ifndef MNG_H
-#define MNG_H
-#include "Linkmake.h"
-int admin_stobook() ;
-int admin_chanbook(usr *temper);
-void usr_mng();
-void usr_designer();
-void usr_main_show(usr *temp);
-#endif
